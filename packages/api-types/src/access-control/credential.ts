409d79c6a0b45fa25f057cf4d3af9e6426421331
MarkALeonard <m.leonard@linc.uk.com>
2025-06-26 15:09:02 +0100

    Add note to AssignedCredential

--- packages/api-types/src/access-control/credential.ts
+++ packages/api-types/src/access-control/credential.ts
@@ -5,6 +5,7 @@ export const sCredentialType = z.enum(['card', 'pin', 'fingerprint']);
 export const sAssignedCredential = z.object({
   type: sCredentialType,
   value: z.string().nonempty(),
+  note: z.string().nullable(),
 });
 
 export type CredentialType = z.infer<typeof sCredentialType>;
