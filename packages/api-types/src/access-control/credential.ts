80007b88ee2f5d66d7f5f4eeca6d13c1c064e012
Yaser Awajan <yaser@simplify9.com>
2025-06-30 13:15:23 +0300

    Modified token value to accept object in addition to string

--- packages/api-types/src/access-control/credential.ts
+++ packages/api-types/src/access-control/credential.ts
@@ -2,12 +2,19 @@ import z from 'zod';
 
 export const sCredentialType = z.enum(['card', 'pin', 'fingerprint']);
 
+export const sCredentialValue = z.union([
+  z.string().nonempty(),
+  z.record(z.unknown()),
+]);
+
 export const sAssignedCredential = z.object({
   type: sCredentialType,
-  value: z.string().nonempty(),
+  value: sCredentialValue,
   note: z.string().nullable(),
 });
 
 export type CredentialType = z.infer<typeof sCredentialType>;
+
+export type CredentialValue = z.infer<typeof sCredentialValue>;
 
 export type AssignedCredential = z.infer<typeof sAssignedCredential>;
