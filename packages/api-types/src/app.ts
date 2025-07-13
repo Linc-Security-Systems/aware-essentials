8b2c6909fa00eb4a6e64e91741813d6d003db294
Yaser Awajan <yaser@simplify9.com>
2025-07-13 14:59:58 +0300

    Updated AppInfo

--- packages/api-types/src/app.ts
+++ packages/api-types/src/app.ts
@@ -3,6 +3,9 @@ import { z } from 'zod';
 export const sAppInfo = z.object({
   version: z.string(),
   releaseDate: z.string(),
+  serverDeviceId: z.string(),
+  alarmDeviceId: z.string(),
+  globalZoneId: z.string(),
 });
 
 export type AppInfo = z.infer<typeof sAppInfo>;
