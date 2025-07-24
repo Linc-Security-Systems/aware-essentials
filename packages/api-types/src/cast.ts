eaacf4de3afeb5746ca20ccadb38fe2de34d7ab2
MarkALeonard <m.leonard@linc.uk.com>
2025-07-24 12:22:34 +0100

    Changed cast request to support stream

--- packages/api-types/src/cast.ts
+++ packages/api-types/src/cast.ts
@@ -1,7 +1,12 @@
 import z from 'zod';
 
-export const sCastCamerasRequest = z.object({
-  cameraIds: z.array(z.string().nonempty()).min(1),
-});
+export const sCastCamerasRequest = z
+  .array(
+    z.object({
+      cameraId: z.string().nonempty(),
+      streamId: z.string().nonempty(),
+    }),
+  )
+  .min(1);
 
 export type CastCamerasRequest = z.infer<typeof sCastCamerasRequest>;
