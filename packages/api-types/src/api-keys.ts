823a9584961e55a4a906a5e2a05c1b694c364ed9
Yaser Awajan <yaser@simplify9.com>
2025-10-23 15:45:20 +0300

    Added 'prefix'

--- packages/api-types/src/api-keys.ts
+++ packages/api-types/src/api-keys.ts
@@ -16,6 +16,7 @@ export const sRevokeApiKeyRequest = z.object({
 export const sApiKeyDto = z.object({
   id: z.string().uuid(),
   displayName: z.string().min(1).max(128),
+  prefix: z.string().min(8).max(16),
   createdOn: z.string().datetime(),
   lastModifiedOn: z.string().datetime(),
 });
