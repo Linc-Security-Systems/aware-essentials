ae517fdde894c6029c60a18b8c450ff505910d22
MarkALeonard <m.leonard@linc.uk.com>
2026-02-10 09:37:03 +0000

    Add face types

--- packages/api-types/src/api/queries/nvr-analytics-server.ts
+++ packages/api-types/src/api/queries/nvr-analytics-server.ts
@@ -102,7 +102,7 @@ export const sObjectLabelResponse = z.array(z.string());
 
 export type ObjectLabelsResponse = z.infer<typeof sObjectLabelResponse>;
 
-// -- Faces
+// -- Faces (Gets list of recognized and un recognized faces in the media library, grouped by person)
 
 export const QUERY_MEDIA_FACES = 'cctv:media-faces';
 
@@ -114,7 +114,7 @@ export const sMediaFacesResponse = z.record(z.string(), z.array(z.string()));
 
 export type MediaFacesResponse = z.infer<typeof sMediaFacesResponse>;
 
-// -- Face People
+// -- Face People (Gets list of people (ids) who are registered in the face recognition system)
 
 export const QUERY_MEDIA_FACE_PEOPLE = 'cctv:media-face-people';
 
@@ -125,6 +125,105 @@ export type MediaFacePeopleArgs = z.infer<typeof sMediaFacePeopleArgs>;
 export const sMediaFacePeopleResponse = z.array(z.string());
 
 export type MediaFacePeopleResponse = z.infer<typeof sMediaFacePeopleResponse>;
+
+// -- Media Face Person Create
+
+export const QUERY_MEDIA_FACE_PERSON_CREATE = 'cctv:media-face-person-create';
+
+export const sMediaFacePersonCreateArgs = z.object({
+  id: z.string().nonempty(),
+});
+
+export type MediaFacePersonCreateArgs = z.infer<
+  typeof sMediaFacePersonCreateArgs
+>;
+
+export const sMediaFacePersonCreateResponse = z.object({
+  success: z.boolean(),
+  message: z.string(),
+});
+
+export type MediaFacePersonCreateResponse = z.infer<
+  typeof sMediaFacePersonCreateResponse
+>;
+
+// -- Media Face Person Delete
+
+export const QUERY_MEDIA_FACE_PERSON_DELETE = 'cctv:media-face-person-delete';
+
+export const sMediaFacePersonDeleteArgs = z.object({
+  id: z.string().nonempty(),
+});
+
+export type MediaFacePersonDeleteArgs = z.infer<
+  typeof sMediaFacePersonDeleteArgs
+>;
+
+export const sMediaFacePersonDeleteResponse = z.object({
+  success: z.boolean(),
+  message: z.string(),
+});
+
+export type MediaFacePersonDeleteResponse = z.infer<
+  typeof sMediaFacePersonDeleteResponse
+>;
+
+// -- Media Face Person Add
+
+export const QUERY_MEDIA_FACE_PERSON_ADD = 'cctv:media-face-person-add';
+
+export const sMediaFacePersonAddArgs = z.object({
+  id: z.string().nonempty(),
+  image: z.string().nonempty(),
+});
+
+export type MediaFacePersonAddArgs = z.infer<typeof sMediaFacePersonAddArgs>;
+
+export const sMediaFacePersonAddResponse = z.object({
+  success: z.boolean(),
+  message: z.string(),
+});
+
+export type MediaFacePersonAddResponse = z.infer<
+  typeof sMediaFacePersonAddResponse
+>;
+
+// -- Media Face Person Remove
+
+export const QUERY_MEDIA_FACE_PERSON_REMOVE = 'cctv:media-face-person-remove';
+
+export const sMediaFacePersonRemoveArgs = z.object({
+  id: z.string().nonempty(),
+  image: z.string().nonempty(),
+});
+
+export type MediaFacePersonRemoveArgs = z.infer<
+  typeof sMediaFacePersonRemoveArgs
+>;
+
+export const sMediaFacePersonRemoveResponse = z.object({
+  success: z.boolean(),
+  message: z.string(),
+});
+
+export type MediaFacePersonRemoveResponse = z.infer<
+  typeof sMediaFacePersonRemoveResponse
+>;
+
+// -- Media Face Image
+
+export const QUERY_MEDIA_FACE_IMAGE = 'cctv:media-face-image';
+
+export const sMediaFaceImageArgs = z.object({
+  id: z.string().nonempty(),
+  imageId: z.string().nonempty(),
+});
+
+export type MediaFaceImageArgs = z.infer<typeof sMediaFaceImageArgs>;
+
+export const sMediaFaceImageResponse = sFileResponse;
+
+export type MediaFaceImageResponse = z.infer<typeof sMediaFaceImageResponse>;
 
 // SCHEMA DICTIONARIES AND TYPE MAPS
 
@@ -137,6 +236,11 @@ export const nvrAnalyticsRequestSchemas = {
   [QUERY_OBJECT_LABELS]: sObjectLabelsArgs,
   [QUERY_MEDIA_FACES]: sMediaFacesArgs,
   [QUERY_MEDIA_FACE_PEOPLE]: sMediaFacePeopleArgs,
+  [QUERY_MEDIA_FACE_PERSON_CREATE]: sMediaFacePersonCreateArgs,
+  [QUERY_MEDIA_FACE_PERSON_DELETE]: sMediaFacePersonDeleteArgs,
+  [QUERY_MEDIA_FACE_PERSON_ADD]: sMediaFacePersonAddArgs,
+  [QUERY_MEDIA_FACE_PERSON_REMOVE]: sMediaFacePersonRemoveArgs,
+  [QUERY_MEDIA_FACE_IMAGE]: sMediaFaceImageArgs,
 } as const;
 
 // Dictionary of response schemas by query type
@@ -148,6 +252,11 @@ export const nvrAnalyticsResponseSchemas = {
   [QUERY_OBJECT_LABELS]: sObjectLabelResponse,
   [QUERY_MEDIA_FACES]: sMediaFacesResponse,
   [QUERY_MEDIA_FACE_PEOPLE]: sMediaFacePeopleResponse,
+  [QUERY_MEDIA_FACE_PERSON_CREATE]: sMediaFacePersonCreateResponse,
+  [QUERY_MEDIA_FACE_PERSON_DELETE]: sMediaFacePersonDeleteResponse,
+  [QUERY_MEDIA_FACE_PERSON_ADD]: sMediaFacePersonAddResponse,
+  [QUERY_MEDIA_FACE_PERSON_REMOVE]: sMediaFacePersonRemoveResponse,
+  [QUERY_MEDIA_FACE_IMAGE]: sMediaFaceImageResponse,
 } as const;
 
 // TypeScript mapping types for requests and responses
@@ -159,6 +268,11 @@ export type NvrAnalyticsQueryRequestMap = {
   [QUERY_OBJECT_LABELS]: ObjectLabelsArgs;
   [QUERY_MEDIA_FACES]: MediaFacesArgs;
   [QUERY_MEDIA_FACE_PEOPLE]: MediaFacePeopleArgs;
+  [QUERY_MEDIA_FACE_PERSON_CREATE]: MediaFacePersonCreateArgs;
+  [QUERY_MEDIA_FACE_PERSON_DELETE]: MediaFacePersonDeleteArgs;
+  [QUERY_MEDIA_FACE_PERSON_ADD]: MediaFacePersonAddArgs;
+  [QUERY_MEDIA_FACE_PERSON_REMOVE]: MediaFacePersonRemoveArgs;
+  [QUERY_MEDIA_FACE_IMAGE]: MediaFaceImageArgs;
 };
 
 export type NvrAnalyticsQueryResponseMap = {
@@ -169,4 +283,9 @@ export type NvrAnalyticsQueryResponseMap = {
   [QUERY_OBJECT_LABELS]: ObjectLabelsResponse;
   [QUERY_MEDIA_FACES]: MediaFacesResponse;
   [QUERY_MEDIA_FACE_PEOPLE]: MediaFacePeopleResponse;
+  [QUERY_MEDIA_FACE_PERSON_CREATE]: MediaFacePersonCreateResponse;
+  [QUERY_MEDIA_FACE_PERSON_DELETE]: MediaFacePersonDeleteResponse;
+  [QUERY_MEDIA_FACE_PERSON_ADD]: MediaFacePersonAddResponse;
+  [QUERY_MEDIA_FACE_PERSON_REMOVE]: MediaFacePersonRemoveResponse;
+  [QUERY_MEDIA_FACE_IMAGE]: MediaFaceImageResponse;
 };
