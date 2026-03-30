2047f62017ef5e84023119e1ef89963daad541b6
Yaser Awajan <yaser@simplify9.com>
2026-03-30 19:25:25 +0300

    NVR Recorder state object

--- packages/api-types/src/objects/device/nvr-recorder.ts
+++ packages/api-types/src/objects/device/nvr-recorder.ts
@@ -5,8 +5,16 @@ export const NVR_RECORDER = 'nvr-recorder';
 export const sRecorderSpecs = z.object({});
 export type RecorderSpecs = z.infer<typeof sRecorderSpecs>;
 
+export const sRecorderStreamStateDto = z.object({
+  upstream: z.enum(['connecting', 'connected', 'disconnected', 'error']),
+  isRecording: z.boolean(),
+});
+
+export type RecorderStreamStateDto = z.infer<typeof sRecorderStreamStateDto>;
+
 export const sRecorderStateDto = z.object({
   connected: z.boolean(),
+  cameras: z.record(z.string(), sRecorderStreamStateDto),
 });
 
 export type RecorderStateDto = z.infer<typeof sRecorderStateDto>;
