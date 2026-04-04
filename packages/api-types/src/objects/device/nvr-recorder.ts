a35f5d1195ff767b7229483a1a27717b853cbaa1
Yaser Awajan <yaser@simplify9.com>
2026-04-04 17:47:26 +0300

    Adjusted recorder state dto

--- packages/api-types/src/objects/device/nvr-recorder.ts
+++ packages/api-types/src/objects/device/nvr-recorder.ts
@@ -9,12 +9,19 @@ export const sRecorderStreamStateDto = z.object({
   upstreamState: z.enum(['connecting', 'connected', 'disconnected', 'error']),
   upstreamError: z.string().nonempty().nullable(),
   isRecording: z.boolean(),
+  head: z.number().nullable(),
+  tail: z.number().nullable(),
+  totalSize: z.number().nullable(),
 });
 
 export type RecorderStreamStateDto = z.infer<typeof sRecorderStreamStateDto>;
 
 export const sRecorderStateDto = z.object({
   connected: z.boolean(),
+  availableDiskSpace: z.number().nullable(),
+  totalDiskSpace: z.number().nullable(),
+  highWatermark: z.number().nullable(),
+  lowWatermark: z.number().nullable(),
   cameras: z.record(z.string(), z.record(z.string(), sRecorderStreamStateDto)),
 });
 
